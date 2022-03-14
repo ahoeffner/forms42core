5f392f198312421e5e6f3c4ab43f7a046fe9d84a
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2022-03-15 00:55:49 +0100

    .

--- src/application/Window.ts
+++ src/application/Window.ts
@@ -5,9 +5,20 @@ export class Window
     private layer:number = 2;
     private modal:HTMLDivElement = null;
     private window:HTMLDivElement = null;
+    private container:HTMLDivElement = null;
 
     constructor(component:Element)
     {
+        let layout:string = Properties.window.page;
+        let template:HTMLTemplateElement = document.createElement("template");
+
+        template.innerHTML = layout;
+        this.window = template.content.querySelector(".window-handle");
+        this.container = template.content.querySelector(".window-page-content");
+
+        //document.importNode(this.window);
+        this.container.appendChild(component);
+        this.window.addEventListener("mousedown",(event) => {this.dragstart(event)});
     }
 
     public block() : void
@@ -25,5 +36,14 @@ export class Window
     public getPage() : Element
     {
         return(this.window);
+    }
+
+
+    private move = false;
+    private mouse = {x: 0, y: 0};
+
+    private dragstart(event:any) : void
+    {
+        console.log("dragstart");
     }
 }
--- src/application/properties/Window.ts
+++ src/application/properties/Window.ts
@@ -2,34 +2,32 @@ export class Window
 {
     public page:string =
     `
-    <head>
-        <style>
-            .window-handle
-            {
-                resize: both;
-                overflow:hidden;
-                position: absolute;
-            }
+    <style>
+        .window-handle
+        {
+            resize: both;
+            overflow:hidden;
+            position: absolute;
+        }
 
-            .window-page-content
-            {
-                top: 0;
-                left: 0;
-                position: relative;
-                width: fit-content;
-                height: fit-content;
-            }
+        .window-page-content
+        {
+            top: 0;
+            left: 0;
+            position: relative;
+            width: fit-content;
+            height: fit-content;
+        }
 
-            .window-modal-block
-            {
-                top: 0;
-                left: 0;
-                width: 0;
-                height: 0;
-                position: absolute;
-            }
-        </style>
-    </head>
+        .window-modal-block
+        {
+            top: 0;
+            left: 0;
+            width: 0;
+            height: 0;
+            position: absolute;
+        }
+    </style>
 
     <div class="window-handle">
         <div class="window-modal-block"></div>

a09e564f4ef4bfe85655f06a9b11d870750cc9e2
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2022-03-22 00:01:35 +0100

    .

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 const version = "2.1.1";
 console.log("Version "+version);
 export { Form } from './src/forms/Form.js';
+export { Canvas, Frame } from './src/application/interfaces/Canvas';
 export { HTMLFragment as Include } from './src/application/HTMLFragment.js';
 export { Component, ModuleDefinition, FormsModule } from './src/application/FormsModule.js';
--- public-api.d.ts
+++ public-api.d.ts
@@ -1,3 +1,4 @@
 export { Form } from './src/forms/Form.js';
+export { Canvas, Frame } from './src/application/interfaces/Canvas';
 export { HTMLFragment as Include } from './src/application/HTMLFragment.js';
 export { Component, ModuleDefinition, FormsModule } from './src/application/FormsModule.js';

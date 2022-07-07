d046a2238efae1d5d219b9c1731d07faf8e2c492
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2022-07-07 14:47:24 +0200

    Update Logger.ts

--- src/application/Logger.ts
+++ src/application/Logger.ts
@@ -9,15 +9,16 @@ export enum Type
 
 export class Logger
 {
+    public static all:boolean = false;
     public static htmlparser:boolean = false;
     public static eventparser:boolean = false;
     public static classloader:boolean = false;
     public static formbinding:boolean = false;
-    public static eventhandling:boolean = true;
+    public static eventhandling:boolean = false;
 
     public static log(type:Type, msg:string) : void
     {
         let flag:string = Type[type];
-        if (Logger[flag]) console.log(msg);
+        if (Logger[flag] || Logger.all) console.log(msg);
     }
 }

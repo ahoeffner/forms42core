92ad52f7766f4d0883d1980cb2abe5b5f1e263bb
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2022-07-17 10:16:08 +0200

    Update Display.ts

--- src/view/fields/implementations/Display.ts
+++ src/view/fields/implementations/Display.ts
@@ -149,6 +149,11 @@ export class Display implements FieldImplementation, EventListenerObject
 			return;
 
 		this.clear();
+
+		test if text or element
+		don't apply properties if not view..
+		Event test on sign and subs with the current key
+
 		if (value == null) value = "";
 		this.element.textContent = value;
 	}

bf8769d1dcb2eb2c9d1580bfd6795ea45e4560a4
Irodans <jobjel@mail.dk>
2022-09-13 15:36:32 +0200

    popup

--- src/application/properties/Popup.ts
+++ src/application/properties/Popup.ts
@@ -23,36 +23,51 @@ export class Popup
 
 	public static footer:string =
 	`
-		<div name="popup-footer" ></div>
+		<div name="popup-footer"></div>
 	`;
 
 	public static PopupStyle =
 	`
-		display: grid;
-		position: relative;
-		width: fit-content;
-		height: fit-content;
+		gap:2px;
+		margin:20px;
+		display:grid;
+		overflow: hidden;
+		padding:10px 20px;
+		align-items: center;
+		align-content: center;
+		justify-content: center;
 	`;
 
+	public static PopupStyleDiv =
+	`
+		margin: 10px 5px;
+		display: inline-flex;
+	`;
+
+	public static PopupStyleLabel =
+	`
+		margin-right: 8px;
+	`;
 
 	public static PopupHeaderStyle =
 	`
 		width: 100%;
 		height: 20px;
 		position: relative;
-		border: 1px solid black;
+		border-bottom: 1px solid black;
 	`;
 
 	public static PopupFooterStyle = null;
 
 	public static PopupCloseButton =
 	`
-		left: 10px;
+		right: 1px;
 		width: 10px;
 		height: 20px;
 		display: grid;
-		position: relative;
-		border: 1px solid black;
+		cursor: default;
+		font-weight: bold;
+		position: absolute;
 	`;
 
 	public static stylePopupWindow(view:HTMLElement) : void
@@ -60,10 +75,15 @@ export class Popup
 		let close:HTMLElement = view.querySelector('div[name="close-button"]');
 		let header:HTMLElement = view.querySelector('div[name="popup-header"]');
 		let footer:HTMLElement = view.querySelector('div[name="popup-footer"]');
+		let style:HTMLElement = view.querySelector('div[name="filter-editor"]');
+		let divstyle:HTMLElement = view.querySelector('div[name="filter-editor"] div');
+		let labelstyle:HTMLElement = view.querySelector('div[name="filter-editor"] label');
 
-		if (Popup.PopupStyle) view.style.cssText = Popup.PopupStyle;
+		if (style && Popup.PopupStyle) style.style.cssText = Popup.PopupStyle;
+		if (divstyle && Popup.PopupStyleDiv) divstyle.style.cssText = Popup.PopupStyleDiv;
 		if (close && Popup.PopupCloseButton) close.style.cssText = Popup.PopupCloseButton;
 		if (header && Popup.PopupHeaderStyle) header.style.cssText = Popup.PopupHeaderStyle;
 		if (footer && Popup.PopupFooterStyle) footer.style.cssText = Popup.PopupFooterStyle;
+		if (labelstyle && Popup.PopupStyleLabel) labelstyle.style.cssText = Popup.PopupStyleLabel;
 	}
 }

6ab6ed60ebb20a0073cc9a79b796d94ed539dd99
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2022-09-17 03:51:17 +0200

    Update QueryByExample.ts

--- src/model/QueryByExample.ts
+++ src/model/QueryByExample.ts
@@ -36,6 +36,7 @@ export class QueryByExample
 		this.table$ = new MemoryTable();
 		this.wrapper$ = new DataSourceWrapper();
 
+		this.wrapper$.block = block;
 		this.wrapper$.source = this.table$;
 		this.record$ = this.wrapper$.create(0);
 		this.record$.status = RecordStatus.QBE;

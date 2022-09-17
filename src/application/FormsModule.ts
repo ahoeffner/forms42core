45e68f1911e045dd66ee9d70777d7fca7996add7
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2022-09-17 14:35:00 +0200

    Update FormsModule.ts

--- src/application/FormsModule.ts
+++ src/application/FormsModule.ts
@@ -137,10 +137,9 @@ export class FormsModule
 		if (!(form.prototype instanceof Form) && !(form.prototype instanceof InternalForm))
 			throw "@Application: Component mapped to '"+form+"' is not a form";
 
-		let canvasimpl:Class<Canvas> = Properties.CanvasImplementationClass;
+		let canvas:Canvas = Properties.CanvasImplementationClass;
 		let factory:ComponentFactory = Properties.FactoryImplementationClass;
 
-		let canvas:Canvas = new canvasimpl();
 		let instance:Form = await factory.createForm(form,parameters);
 
 		instance.canvas = canvas;

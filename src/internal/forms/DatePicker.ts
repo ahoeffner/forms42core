659c2f63751db619c1497fd385ba02c7054a87d4
Irodans <jobjel@mail.dk>
2022-09-20 11:10:57 +0200

    dataPicker

--- src/internal/forms/DatePicker.ts
+++ src/internal/forms/DatePicker.ts
@@ -17,6 +17,7 @@ import { EventType } from "../../control/events/EventType.js";
 import { FormEvent } from "../../control/events/FormEvent.js";
 import { Internals } from "../../application/properties/Internals.js";
 import { DatePicker as Properties } from "../../application/properties/DatePicker.js";
+import { dates } from "../../model/dates/dates.js";
 
 export class DatePicker extends Form
 {
@@ -36,6 +37,8 @@ export class DatePicker extends Form
 			{type: EventType.OnEdit, field: "date"},
 			{type: EventType.WhenValidateField, field: "date"}
 		]);
+
+		this.addEventListener(this.setDay,{type: EventType.Mouse, mouse:MouseMap.click});
 
 		this.addEventListener(this.done,{type: EventType.Key, key: KeyMap.enter});
 		this.addEventListener(this.close,{type: EventType.Key, key: KeyMap.escape});
@@ -63,24 +66,33 @@ export class DatePicker extends Form
 	{
 		let view:HTMLElement = this.getView();
 		Internals.stylePopupWindow(view);
-
+		Properties.styleDatePicker(view)
 		let value:Date = this.parameters.get("value");
 		if (value == null) value = new Date();
-		console.log("value : "+value);
 
 		this.setValue("calendar","prev","<");
 		this.setValue("calendar","next",">");
 		this.setValue("calendar","date",value);
-
+		this.setDate();
 		this.populateDates();
 		return(true);
 	}
 
+	private async setDay(event:FormEvent) : Promise<boolean>
+	{
+		console.log(event.field)
+		this.day = this.getValue(event.block,event.field)
+		this.date.setDate(this.day);
+		this.setValue("calendar","date",this.date);
+		this.populateDates();
+		return(true)
+	}
 
 	private async setDate() : Promise<boolean>
 	{
 		this.date = this.getValue("calendar","date");
-		console.log(this.date)
+		
+		this.populateDates();
 		return(true)
 	}
 
@@ -88,7 +100,7 @@ export class DatePicker extends Form
 	{
 		this.date.setMonth(this.date.getMonth()+1);
 		console.log("next "+this.date);
-		//this.populateDates()
+		this.populateDates();
 		return(true)
 	}
 
@@ -96,54 +108,26 @@ export class DatePicker extends Form
 	{
 		this.date.setMonth(this.date.getMonth()-1);
 		console.log("previous "+this.date);
-		//this.populateDates()
+		this.populateDates();
 		return(true);
 	}
-
 
 	private populateDates() : void
 	{
 		let dayno:number = 0;
+		if(this.date == null) this.date = new Date();	
+		let Lday:string = dates.format(this.date,"MMM-YYYY");
 		let days:number = this.getDaysInMonth(this.year,this.month);
-
-		for (let week = 0; week < 5; week++)
+	
+		this.setValue("calendar","mth",Lday);
+		for (let week = 1; week <= 5; week++)
 		{
-			for (let day = 0; day < 7; day++)
+			for (let day = 1; day <= 7; day++)
 			{
 				if (++dayno < days)
 					this.setValue("calendar","day-"+week+""+day,dayno);
 			}
 		}
-
-		/*
-		//this.days_element.textContent = '';
-
-
-		//Found local from browser
-		let month_output = new Date(this.year,this.month) .toLocaleDateString('en-us', this.options) + ' ' + this.year
-		// this.setValue("calander","mth", month_output)
-		//this.mth_element.textContent = month_output;
-
-		for(let day = 1; day < daysInCurrentMonth + 1; day++)
-		{
-			let day_element = document.createElement('div');
-			day_element.classList.add('day');
-			day_element.textContent = day.toString();
-			if(this.selectedDay == day && this.selectedYear == this.year && this.selectedMonth + 1 == this.month + 1)
-			{
-				this.day = day;
-				day_element.classList.add('selected');
-			}
-			// this.addEventListener(day_element,{block:"calender", field:"days"})
-			day_element.addEventListener('click',() => this.choiceDay(day))
-			//this.days_element.appendChild(day_element)
-		}
-		*/
-	}
-
-	private choiceDay(day:number)
-	{
-		this.populateDates();
 	}
 
 	private  getDaysInMonth(year, month): number
@@ -151,21 +135,20 @@ export class DatePicker extends Form
 		return new Date(year, month,0).getDate();
 	}
 
-
 	public static page:string =
-	Internals.header + Properties.Day +
+	Internals.header +
 	`
 	<div name="popup-body">
-		<div class="date-picker">
+		<div name="date-picker">
 			<div><span>Date</span>:<input name="date" from="calendar" date></div>
-			<div class="dates">
-				<div class="month">
-					<div class="arrows" tabindex="0" name="prev" from="calendar"></div>
-					<div class="mth" name="mth" from="calendar"></div>
-					<div class="arrows" tabindex="0" name="next" from="calendar"></div>
+			<div name="dates">
+				<div name="month">
+					<div tabindex="0" name="prev" from="calendar"></div>
+					<div name="mth" from="calendar"></div>
+					<div tabindex="0" name="next" from="calendar"></div>
 				</div>
-				<div class="week" foreach="week in 1..5">
-					<div class="day" foreach="day in 1..7">
+				<div name="week" foreach="week in 1..5">
+					<div name="day" foreach="day in 1..7">
 						<span tabindex="-1" name="day-$week$day" from="calendar"></span>
 					</div>
 				</div>

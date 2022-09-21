96ff3714ecbdc8d28f167a1cdcaaf473287adb80
Irodans <jobjel@mail.dk>
2022-09-21 23:53:36 +0200

    datapicker 1 week days

--- src/internal/forms/DatePicker.ts
+++ src/internal/forms/DatePicker.ts
@@ -83,13 +83,6 @@ export class DatePicker extends Form
 		return(true);
 	}
 
-
-	private async onClick(event:FormEvent) : Promise<boolean>
-	{
-
-		return(true);
-	}
-
 	private async setDay(event:FormEvent) : Promise<boolean>
 	{
 		if(event.field == null)
@@ -110,7 +103,6 @@ export class DatePicker extends Form
 	private async setDate() : Promise<boolean>
 	{
 		this.date = this.getValue("calendar","date");
-
 		this.populateDates();
 		return(true)
 	}
@@ -136,24 +128,52 @@ export class DatePicker extends Form
 		let dayno:number = 0;
 		if(this.date == null) this.date = new Date();	
 		let Lday:string = dates.format(this.date,"MMM YYYY");
+		let weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 		let days:number = this.getDaysInMonth(this.date.getFullYear(),this.date.getMonth());
+		let firstdaysname:string = this.getDaysNameMonth(this.date.getFullYear(),this.date.getMonth() ,1);
+
+		for (let day = 0; day <= 6; day++) {
+			this.setValue("calendar","weekday-"+ day, weekdays[day])
+		}
 
 		this.setValue("calendar","mth",Lday);
-		for (let week = 1; week <= 5; week++)
+		for (let week = 1; week <= 6; week++)
 		{
 			for (let day = 1; day <= 7; day++)
-			{
-				if (++dayno <= days)
-					this.setValue("calendar","day-"+week+""+day,dayno);
-				else
-					this.setValue("calendar","day-"+week+""+day, " ");
+			{			
+				if(week == 1)
+				{	
+					let theday:number = weekdays.findIndex(element => element == firstdaysname);
+
+					if(theday < day)
+					{
+						++dayno
+						this.setValue("calendar","day-"+week+""+day, dayno);
+					} 
+					else 
+						this.setValue("calendar","day-"+week+""+day, null);
+
+				} 
+				else if(++dayno <= days) 
+				{	
+					this.setValue("calendar","day-"+week+""+day, dayno);
+				}
+				else 
+				{
+					this.setValue("calendar","day-"+week+""+day, null);
+				}
 			}
 		}
 	}
 
+	private getDaysNameMonth(year:number,month:number,day:number) : string
+	{
+		return new Date(`${year}-${month + 1}-${day}`).toLocaleDateString("en-US", {weekday: "short"});
+	}
+
 	private  getDaysInMonth(year:number, month:number): number
 	{
-		return new Date(year, month,0).getDate();
+		return new Date(year, month + 1,0).getDate();
 	}
 
 	public static page:string =
@@ -168,7 +188,12 @@ export class DatePicker extends Form
 					<div name="mth" from="calendar"></div>
 					<div tabindex="0" name="next" from="calendar"></div>
 				</div>
-				<div name="week" foreach="week in 1..5">
+				<div name="week">
+					<div name="day" foreach="weekday in 0..6">
+						<span name="weekday-$weekday" from="calendar"></span>
+					</div>
+				</div>
+				<div name="week" foreach="week in 1..6">
 					<div name="day" foreach="day in 1..7">
 						<span tabindex="-1" name="day-$week$day" from="calendar"></span>
 					</div>

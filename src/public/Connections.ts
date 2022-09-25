42a9545fa72b20936acf0221ef24d7b84e08f3a1
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2022-09-25 22:21:32 +0200

    .

---
+++ src/public/Connections.ts
@@ -0,0 +1,36 @@
+/*
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation.
+
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ */
+
+import { Connection } from "./Connection";
+
+export class Connections
+{
+	private static conns$:Map<string,Connection> =
+		new Map<string,Connection>();
+
+	public static get(name:string) : Connection
+	{
+		return(Connections.conns$.get(name?.toLowerCase()));
+	}
+
+	public static create(name:string, url?:string|URL) : Connection
+	{
+		let conn:Connection = new Connection(name,url);
+		Connections.conns$.set(name?.toLowerCase(),conn);
+		return(conn);
+	}
+
+	public static register(conn:Connection) : void
+	{
+		Connections.conns$.set(conn.name?.toLowerCase(),conn);
+	}
+}

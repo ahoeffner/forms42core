82b4e8a90fceaebe904c7893d7d39db03fce75d2
Irodans <jobjel@mail.dk>
2022-12-23 14:31:34 +0100

    ..

--- src/internal/forms/Alert.ts
+++ src/internal/forms/Alert.ts
@@ -83,12 +83,13 @@ export class Alert extends Form
 		`
 		<div name="popup-body">
 			<div name="msg" from="alert"></div>
-		</div>
-		<div name="lowerright">
-			<div name="buttonarea">
-				<button name="close" onClick="this.close()">Ok</button>
+			<div name="lowerright">
+				<div name="buttonarea">
+					<button name="close" onClick="this.close()">Ok</button>
+				</div>
 			</div>
 		</div>
+	
 		`
 	+ 	Internals.footer;
 }

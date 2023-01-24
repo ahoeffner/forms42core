6762e739a919599b95bca55e161cd9e8180aced1
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2023-01-24 10:16:52 +0100

    Update Block.ts

--- src/view/Block.ts
+++ src/view/Block.ts
@@ -115,7 +115,7 @@ export class Block
 		}
 		else
 		{
-			let state:RecordState = this.model.getRecord().state;
+			let state:RecordState = this.model.getRecord()?.state;
 			let inst:FieldInstance = this.getCurrentRow()?.getFirstInstance(this.convert(state));
 			if (inst == null) inst = this.getRow(-1)?.getFirstInstance(this.convert(state));
 			if (!inst) console.log("No available fields in "+this.name+" in state "+RecordState[state])
@@ -129,7 +129,7 @@ export class Block
 		clazz = clazz?.toLowerCase();
 
 		let inst:FieldInstance = null;
-		let state:RecordState = this.model.getRecord().state;
+		let state:RecordState = this.model.getRecord()?.state;
 		let ifield:Field = this.getCurrentRow().getField(field);
 
 		if (ifield == null)

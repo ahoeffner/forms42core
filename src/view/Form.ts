78953a3adde423f363fdc7dd30a4fecbd04af0e2
ahoeffner <alex@hoeffner.net>
2023-05-23 14:29:06 +0200

    Update Form.ts

--- src/view/Form.ts
+++ src/view/Form.ts
@@ -550,7 +550,6 @@ export class Form implements EventListenerObject
 
 		if (this.curinst$)
 		{
-			this.curinst$.blur(true);
 			if (!field) field = this.curinst$.field.name;
 			if (!block) block = this.curinst$.field.block.name;
 		}
@@ -582,7 +581,6 @@ export class Form implements EventListenerObject
 			return(false);
 		}
 
-		match[0].focus();
 		return(this.keyhandler(key,match[0]));
 	}
 

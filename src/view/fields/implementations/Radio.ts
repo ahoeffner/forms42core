3948e70d3ed834d2e813dd8c878334f5b44083bf
ahoeffner <alex@hoeffner.net>
2023-08-14 15:33:27 +0200

    Update Radio.ts

--- src/view/fields/implementations/Radio.ts
+++ src/view/fields/implementations/Radio.ts
@@ -202,12 +202,6 @@ export class Radio implements FieldImplementation, EventListenerObject
       let bubble:boolean = false;
 		this.event.setEvent(event);
 
-		if (this.event.type == "wait")
-			await this.event.wait();
-
-		if (this.event.waiting)
-			return;
-
 		if (this.event.type == "focus")
 			bubble = true;
 
